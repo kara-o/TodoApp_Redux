1344ec051d935662a6b649bc2651fe010967aae5
kara-o <44376276+kara-o@users.noreply.github.com>
2020-02-28 22:31:03 -0800

    connected to node server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  useEffect(() => {
+    callBackendAPI()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
+
   return (
     <div>
       <h1>My ToDo List</h1>
