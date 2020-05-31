f9f8ad7a410b6483aa109a4bebcb07dcaa339adc
kara-o <44376276+kara-o@users.noreply.github.com>
2020-05-30 18:04:10 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
-import LoginUser from './components/LoginUser';
+import React, { useState } from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import LoginUser from "./components/LoginUser";
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(null);
-
-  const loginUser = ({ username, email }) => {
-    setLoggedIn(username);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const loginUser = async ({ username, email }) => {
+    try {
+      const config = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+        }),
+      };
+      const response = await fetch(`/api/users/login`, config);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setLoggedIn(data);
+        return data;
+      } else {
+        throw new Error("There was an error logging in.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div>
       {loggedIn ? (
         <>
-          <h1>Welcome back, {loggedIn}.</h1>
+          <h1>Welcome, {loggedIn.username}.</h1>
           <AddTodo />
           <TodoList />
         </>
