fc3bb84afc3035f36135a99a595505b2462a13b1
kara-o <44376276+kara-o@users.noreply.github.com>
2020-05-31 12:54:53 -0700

    fetch todos w id

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import LoginUser from "./components/LoginUser";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+
+  const getTodos = async (id) => {
+    try {
+      const response = await fetch(`/api/todos?user_id=${id}`);
+      if (response.ok) {
+        const todos = await response.json();
+        console.log("todos: ", todos);
+        return todos;
+      } else {
+        throw new Error("There was an error getting the user's todos.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const loginUser = async ({ username, email }) => {
     try {
       const config = {
@@ -20,10 +36,12 @@ const App = () => {
       };
       const response = await fetch(`/api/users/login`, config);
       if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        setLoggedIn(data);
-        return data;
+        const user = await response.json();
+        console.log("user: ", user);
+
+        const todos = await getTodos(user.id);
+
+        setLoggedIn({ user, todos });
       } else {
         throw new Error("There was an error logging in.");
       }
